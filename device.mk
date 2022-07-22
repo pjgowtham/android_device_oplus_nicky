f67638bb5ad574f3a3eada843efa2488e7e006f3
dev-harsh1998 <dev-harsh1998@hotmail.com>
2022-07-22 12:22:13 +0530

    nicky: Define custom light sensor for RMX1901/RMX1921

--- device.mk
+++ device.mk
@@ -299,6 +299,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
+PRODUCT_PACKAGES += \
+    OPlusAmoledFrameworksResTarget
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
